ec3d6616826d4610f7202da7210d5f04f71a4edb
thanh5320 <thanh20183632@gmail.com>
2025-01-16 10:19:35 +0700

    cicd init

--- src/test/java/com/example/luklak_api/TestMainVerticle.java
+++ src/test/java/com/example/luklak_api/TestMainVerticle.java
@@ -1,5 +1,6 @@
 package com.example.luklak_api;
 
+import com.example.luklak_api.verticle.ApiVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
@@ -12,7 +13,7 @@ public class TestMainVerticle {
 
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    vertx.deployVerticle(new ApiVerticle(), testContext.succeeding(id -> testContext.completeNow()));
   }
 
   @Test
